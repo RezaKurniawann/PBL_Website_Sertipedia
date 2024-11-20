ab803de11089d44844a4c1568c8d41a35254e3e8
InggaChintia <inggainggachintia@gmail.com>
2024-11-20 10:23:07 +0700

    update landingpage

--- app/Http/Controllers/UserHomeController.php
+++ app/Http/Controllers/UserHomeController.php
@@ -11,20 +11,74 @@ class UserHomeController extends Controller
         // Contoh data dosen
         $dosen = [
             [
-                'nama' => 'Nama Dosen, S.ST., MT.',
-                'profesi' => 'lorem ipsum',
-                'keahlian' => 'lorem ipsum',
-                'foto' => 'images/profile-placeholder.png' // Path gambar default
-            ],
-            [
-                'nama' => 'Nama Dosen, S.ST., MT.',
-                'profesi' => 'lorem ipsum',
-                'keahlian' => 'lorem ipsum',
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Teknologi Informasi',
+                'keahlian' => 'Artificial Intelligence',
                 'foto' => 'images/profile-placeholder.png'
             ],
-            // Tambahkan data dosen lainnya
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Guru Besar',
+                'keahlian' => 'Data Science',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Teknik Informatika',
+                'keahlian' => 'Network Security',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Sistem Informasi',
+                'keahlian' => 'Enterprise Systems',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Teknik Informatika',
+                'keahlian' => 'Web Development',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Multimedia',
+                'keahlian' => 'Graphic Design',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Teknologi Informasi',
+                'keahlian' => 'Mobile Development',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Sistem Informasi',
+                'keahlian' => 'Database Management',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Teknik Informatika',
+                'keahlian' => 'Cloud Computing',
+                'foto' => 'images/profile-placeholder.png'
+            ],
+            [
+                'nama' => 'Nama Dosen, S.ST., M.T.',
+                'profesi' => 'Dosen Sistem Informasi Bisnis',
+                'keahlian' => 'Big Data Analytics',
+                'foto' => 'images/profile-placeholder.png'
+            ],
         ];
 
-        return view('user.index', compact('dosen'));
+         // Variabel activeMenu untuk menandai menu yang aktif
+         $activeMenu = 'home';
+         $breadcrumb = (object) [
+            'title' => 'Home',
+            'list' => ['Home', 'home']
+        ];
+
+        return view('user.index', compact('dosen', 'activeMenu', 'breadcrumb'));
     }
 }
