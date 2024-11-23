dc382d77168229f4e095e9d08fad7af2e8eb28c1
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-11-23 15:48:49 +0700

    crud event dan level

---
+++ resources/views/admin/event/pelatihan/confirm.blade.php
@@ -0,0 +1,101 @@
+
+@empty($pelatihan)
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-danger">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                    Data yang anda cari tidak ditemukan
+                </div>
+                <a href="{{ url('manage/event/pelatihan') }}" class="btn btn-warning">Kembali</a>
+            </div>
+        </div>
+    </div>
+@else
+    <form action="{{ url('manage/event/pelatihan/' . $pelatihan->id_pelatihan . '/delete_ajax') }}" method="POST" id="form-delete">
+        @csrf
+        @method('DELETE')
+        <div id="modal-master" class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data Pelatihan</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-warning">
+                        <h5><i class="icon fas fa-ban"></i> Konfirmasi !!!</h5>
+                        Apakah Anda ingin menghapus data seperti di bawah ini?
+                    </div>
+                    <table class="table table-sm table-bordered table-striped"> 
+                        <tr><th class="text-right col-3">Nama Pelatihan :</th><td class="col-9">{{ $pelatihan->nama }}</td></tr>
+                        <tr><th class="text-right col-3">Vendor : </th><td class="col-9">{{ $pelatihan->vendor->nama }}</td></tr>
+                        <tr><th class="text-right col-3">Kuota : </th><td class="col-9">{{ $pelatihan->kuota }}</td></tr>
+                        <tr><th class="text-right col-3">Lokasi : </th><td class="col-9">{{ $pelatihan->lokasi }}</td></tr>
+                        <tr><th class="text-right col-3">Biaya : </th><td class="col-9">Rp {{ number_format($pelatihan->biaya, 2, ',', '.') }}</td></tr>
+                        <tr><th class="text-right col-3">Level Pelatihan : </th><td class="col-9">{{ $pelatihan->level_pelatihan }}</td></tr>
+                        <tr><th class="text-right col-3">Tanggal Awal : </th><td class="col-9">{{ \Carbon\Carbon::parse($pelatihan->tanggal_awal)->format('d/m/Y') }}</td></tr>
+                        <tr><th class="text-right col-3">Tanggal Akhir : </th><td class="col-9">{{ \Carbon\Carbon::parse($pelatihan->tanggal_akhir)->format('d/m/Y') }}</td></tr>
+                        <tr><th class="text-right col-3">Periode : </th><td class="col-9">{{ $pelatihan->periode->tahun }}</td></tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                    <button type="submit" class="btn btn-primary">Ya, Hapus</button>
+                </div>
+            </div>
+        </div>
+    </form>
+    <script>
+        $(document).ready(function() {
+            $("#form-delete").validate({
+                rules: {},
+                submitHandler: function(form) {
+                    $.ajax({
+                        url: form.action,
+                        type: form.method,
+                        data: $(form).serialize(),
+                        success: function(response) {
+                            if (response.status) {
+                                $('#myModal').modal('hide');
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Berhasil',
+                                    text: response.message
+                                });
+                                datapelatihan.ajax.reload();
+                            } else {
+                                $('.error-text').text('');
+                                $.each(response.msgField, function(prefix, val) {
+                                    $('#error-' + prefix).text(val[0]);
+                                });
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Terjadi Kesalahan',
+                                    text: response.message
+                                });
+                            }
+                        }
+                    });
+                    return false;
+                },
+                errorElement: 'span',
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
+                },
+                highlight: function(element, errorClass, validClass) {
+                    $(element).addClass('is-invalid');
+                },
+                unhighlight: function(element, errorClass, validClass) {
+                    $(element).removeClass('is-invalid');
+                }
+            });
+        });
+    </script>
+@endempty
--- resources/views/admin/level/index.blade.php
+++ resources/views/admin/level/index.blade.php
@@ -1,11 +1,107 @@
-@extends ('layouts.template' )
-
+@extends('layouts.template')
 @section('content')
-
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Halo, Selamat Datang!</h3>
-        <div class="card-tools"></div>
-</div>
-
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <button onclick="modalAction('{{ url('manage/level/import') }}')"
+                    class="btn btn-sm btn-info mt-1 ">
+                    <i class="fa fa-upload"></i> Import Level
+                </button>
+                <a href="{{ url('manage/level/export_excel') }}" class="btn btn-sm btn-success mt-1 ">
+                    <i class="fa fa-file-excel"></i> Export Excel
+                </a>
+                <a href="{{ url('manage/level/export_pdf') }}" class="btn btn-sm btn-danger mt-1 ">
+                    <i class="fa fa-file-pdf"></i> Export PDF
+                </a>
+                <button onclick="modalAction('{{ url('manage/level/create_ajax') }}')"
+                    class="btn btn-sm btn-primary mt-1 ">
+                    <i class="fa fa-plus"></i> Tambah Data
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode</th>
+                        <th>Nama Level</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 @endsection
+@push('css')
+<style>
+    .card {
+        border-radius: 10px;
+        overflow: hidden;
+    }
+
+    .btn {
+        transition: all 0.3s ease;
+    }
+
+    .btn:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+    }
+
+    .table-hover tbody tr:hover {
+        background-color: rgba(0, 123, 255, 0.1);
+    }
+
+</style>
+@endpush
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        $(document).ready(function() {
+            var dataLevel = $('#table_level').DataTable({
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('manage/level/list') }}",
+                    "dataType": "json",
+                    "type": "POST"
+                },
+                columns: [{
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "kode",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                }, {
+                    data: "nama",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                }, {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }]
+            });
+        });
+    </script>
+@endpush
