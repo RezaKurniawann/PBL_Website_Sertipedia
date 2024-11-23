ce1cd0ac21a5aee65fd29b12473082deb1357cf4
Carieska Berliana Novita Kusuma Wardani <riskaberliana123@gmail.com>
2024-11-23 16:27:29 +0700

    template different

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -16,8 +16,7 @@
     <link rel="stylesheet" href="{{ url ('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- DataTables -->
     <link rel="stylesheet" href="{{ url ('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet"
-        href="{{ url ('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ url ('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ url ('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
     {{-- Sweet Alert2 --}}
     <link rel="stylesheet" href="{{ url ('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
