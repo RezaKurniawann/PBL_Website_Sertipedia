ed6f823d9a078e3ceb7d83f11387896b6219ec55
AhmadHidayatP <ahmadhidayatpermana202@gmail.com>
2024-11-27 11:01:26 +0700

    Revert "Update Rekomendasi"
    This reverts commit 801f17367480124c7b927011b632f481e5cb1160.

--- app/Http/Controllers/VerifikasiPelatihanController.php
+++ app/Http/Controllers/VerifikasiPelatihanController.php
@@ -1,50 +1,34 @@
 <?php
-namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
 use App\Models\MataKuliahModel;
 use App\Models\BidangMinatModel;
 use App\Models\PeriodeModel;
 use App\Models\VendorModel;
-use App\Models\PelatihanModel; // Tambahkan Model untuk m_pelatihan
-use App\Models\DetailPelatihanModel; // Tambahkan Model untuk detail_pelatihan
 
 use Illuminate\Http\Request;
 
 class VerifikasiPelatihanController extends Controller
 {
     public function index()
-    {
-        $breadcrumb = (object) [
-            'title' => 'Daftar Pengajuan Pelatihan',
-            'list' => ['Home', 'Pelatihan']
-        ];
-
-        $page = (object) [
-            'title' => 'Daftar Pengajuan Pelatihan'
-        ];
-
-        $activeMenu = 'verifikasi';
-        $activeSubMenu = 'pelatihan';
-
-        // Data untuk dropdown
-        $matakuliah = MataKuliahModel::all();
-        $bidangminat = BidangMinatModel::all();
-        $periode = PeriodeModel::all();
-        $vendor = VendorModel::all();
-
-        // Query untuk data pelatihan
-        $pelatihan = DetailPelatihanModel::with(['pelatihan', 'user'])
-            ->get();
-
-        return view('user.pimpinan.verifikasi.pelatihan', [
-            'breadcrumb' => $breadcrumb,
-            'page' => $page,
-            'matakuliah' => $matakuliah,
-            'bidangminat' => $bidangminat,
-            'periode' => $periode,
-            'vendor' => $vendor,
-            'pelatihan' => $pelatihan, 
-            'activeMenu' => $activeMenu
-        ]);
-    }
+     {
+         $breadcrumb = (object) [
+             'title' => 'Daftar Pengajuan Pelatihan',
+             'list' => ['Home', 'Pelatihan']
+         ];
+ 
+         $page = (object) [
+             'title' => 'Daftar Pengajuan Pelatihan'
+         ];
+ 
+         $activeMenu = 'verifikasi';
+         $activeSubMenu = 'pelatihan';
+         
+         $matakuliah = MataKuliahModel::all();
+         $bidangminat = BidangMinatModel::all();
+         $periode = PeriodeModel::all();
+         $vendor = VendorModel::all();
+         
+         return view('user.pimpinan.verifikasi.pelatihan', ['breadcrumb' => $breadcrumb, 'page' => $page, 'matakuliah' => $matakuliah, 'bidangminat' => $bidangminat, 'periode' => $periode, 'vendor' => $vendor, 'activeMenu' => $activeMenu]);
+     }
 }
