e02ad91c69b4daf36a79002a979dda2fe1182042
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-12-15 13:42:04 +0700

    config kernel cors api

--- routes/api.php
+++ routes/api.php
@@ -81,3 +81,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::options('{any}', function () {
+    return response()->json(['status' => 'OK'], 200);
+})->where('any', '.*');
+
+
